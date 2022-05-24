d0e883ae955f69ca570a83748b403154b3784446
FrancOlano <olanofranco1@gmail.com>
2022-05-24 12:02:43 +0200

    login base

--- franco/src/App.js
+++ franco/src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="total">
+      <header>LOGIN</header>
+        <div class="card principal">
+          <div class="mb-3">
+            <label for="formGroupExampleInput" class="form-label">Email</label>
+            <input type="text" class="border-bottom border-0 border-secondary" id="formGroupExampleInput" placeHolder="ej: email@gmail.com"/>
+          </div>
+          <div class="mb-3">
+            <label for="formGroupExampleInput2" class="form-label">Contraseña</label>
+            <input type="password" class="border-bottom border-0 border-secondary" id="formGroupExampleInput2" placeHolder=".............."/>
+          </div>
+
+      </div>
     </div>
   );
 }
